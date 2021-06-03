13cd237870a1af31be12f1e873398b528329ebab
Godicks <matvej.novik@gmail.com>
2021-06-03 21:53:19 +0300

    some fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const getRange = (min, max) => {
   if (min < 0 || min > max) {
-   throw new Error('something wrong, check numbers')
+   throw new Error('something wrong, check numbers');
   }
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
