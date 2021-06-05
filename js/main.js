1c7b8f90fa26e265b0e693ad08b9a32596d3a28f
Godicks <matvej.novik@gmail.com>
2021-06-05 08:24:02 +0300

    add toFixed function

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const getRange = (min, max) => {
   if (min < 0 || min > max) {
    throw new Error('something wrong, check numbers');
   }
-  return Math.floor(min + Math.random() * (max + 1 - min));
+  return (min + Math.random() * (max + 1 - min)).toFixed(2);
 };
